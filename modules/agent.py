02b9bb70de92947042feb33d081e51d8dfefc7ff
Ashish <aknexa@gmail.com>
2026-06-04 10:37:16 +0530

    modules.agent.py changed

--- modules/agent.py
+++ modules/agent.py
@@ -5,11 +5,16 @@ from langchain_core.output_parsers import StrOutputParser
 from modules.tools import web_search
 from modules.prompts import writer_prompt, reviewer_prompt, revision_prompt
 from dotenv import load_dotenv
+import os 
+import os
 
 # Load env 
 load_dotenv()
 
-# Define llm 
+print("GROQ KEY FOUND:", bool(os.getenv("GROQ_API_KEY")))
+print("TAVILY KEY FOUND:", bool(os.getenv("TAVILY_API_KEY")))
+
+# Define llm
 llm = init_chat_model(model="llama-3.1-8b-instant",
                       model_provider="groq",
                       temperature = 0)
